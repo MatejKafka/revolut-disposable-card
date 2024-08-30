0d35900b817eb8925642a702a26292bbe340e2c1
Matej Kafka <git@a-n.cc>
2024-08-30 04:25:08 +0200

    Remove lib debug prints, switch from explicit promises to async/await

--- lib/RevolutInternalAPI/index.ts
+++ lib/RevolutInternalAPI/index.ts
@@ -3,7 +3,7 @@ import crypto from "node:crypto";
 
 import * as Responses from './responses';
 import * as Requests from './requests';
-import URLs from './urls';
+import urls from './urls';
 
 const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36';
 
@@ -51,90 +51,78 @@ export default class Revolut {
         });
     }
 
-    private setToken(): Promise<boolean> {
-        return new Promise(async (resolve, reject) => {
-            let signinPayload: Requests.T_SignIn = {
-                phone: this.phoneNumber!,
-                password: this.password,
-                channel: 'APP',
-            };
+    private async setToken(): Promise<void> {
+        let signinPayload: Requests.T_SignIn = {
+            phone: this.phoneNumber!,
+            password: this.password,
+            channel: 'APP',
+        };
 
-            try {
-                let res = await this.axios.post(URLs.signin, signinPayload);
-                let data: Responses.T_SignIn = res.data;
-                this.tokenId = data.tokenId;
-
-                resolve(true);
-            } catch (ex) {
-                if (ex instanceof AxiosError) {
-                    console.log(ex.response?.data);
-                    reject(ex.response?.data);
-                }
+        try {
+            let res = await this.axios.post(urls.signin, signinPayload);
+            let data: Responses.T_SignIn = res.data;
+            this.tokenId = data.tokenId;
+        } catch (ex) {
+            if (ex instanceof AxiosError) {
+                throw ex.response?.data;
             }
-        });
+        }
     }
 
-    private getPublicToken(): Promise<Responses.T_Token> {
-        return new Promise(async (resolve) => {
-            try {
-                let payload: Requests.T_Token = {
-                    phone: this.phoneNumber!,
-                    password: this.password,
-                    tokenId: this.tokenId,
-                };
+    private async getPublicToken(): Promise<Responses.T_Token> {
+        try {
+            let payload: Requests.T_Token = {
+                phone: this.phoneNumber!,
+                password: this.password,
+                tokenId: this.tokenId,
+            };
 
-                let res = await this.axios.post(URLs.token, payload);
-                let data: Responses.T_Token = res.data;
+            let res = await this.axios.post(urls.token, payload);
+            let data: Responses.T_Token = res.data;
 
-                let credentials = (res.headers['set-cookie'] || ['.=;.'])[0].split(';')[0].split('=')[1] + '=';
-                let refreshToken = (res.headers['set-cookie'] || ['.=;.'])[1].split(';')[0].split('=')[1] + '=';
+            let credentials = (res.headers['set-cookie'] || ['.=;.'])[0].split(';')[0].split('=')[1] + '=';
+            let refreshToken = (res.headers['set-cookie'] || ['.=;.'])[1].split(';')[0].split('=')[1] + '=';
 
-                this.credentials = credentials;
-                this.refreshToken = refreshToken;
+            this.credentials = credentials;
+            this.refreshToken = refreshToken;
 
-                if (credentials == '' || refreshToken == '') {
-                    throw new Error('Missing credentials or refresh token');
-                    return;
-                }
-
-                this.configFn({
-                    credentials,
-                    refreshToken,
-                    deviceId: this.deviceId,
-                    data,
-                });
-
-                resolve(data);
-            } catch (ex) {
-                if (ex instanceof AxiosError) {
-                    let data: Responses.T_Token = ex.response?.data;
-                    resolve(data);
-                }
+            if (credentials == '' || refreshToken == '') {
+                throw new Error('Missing credentials or refresh token');
             }
-        });
+
+            this.configFn({
+                credentials,
+                refreshToken,
+                deviceId: this.deviceId,
+                data,
+            });
+
+            return data;
+        } catch (ex) {
+            if (ex instanceof AxiosError) {
+                return <Responses.T_Token>ex.response?.data;
+            }
+            throw ex;
+        }
     }
 
     private waitForPublicToken(): Promise<Responses.T_Token> {
         return new Promise(async (resolve, reject) => {
-            try {
-                let interval = setInterval(async () => {
-                    let data = await this.getPublicToken();
-                    console.log(data);
-                    if (data.tokenExpiryDate) {
-                        clearInterval(interval);
+            let interval = setInterval(async () => {
+                let data = await this.getPublicToken();
+                if (data.tokenExpiryDate) {
+                    clearInterval(interval);
+                    resolve(data);
+                    return;
+                }
+                if (data.code != Responses.E_ErrorCodes.USER_HASNT_PROVIDED_CONSENT) {
+                    clearInterval(interval);
+                    reject(data);
+                    return;
+                }
 
-                        resolve(data);
-                        return;
-                    }
-                    if (data.code != Responses.E_ErrorCodes.USER_HASNT_PROVIDED_CONSENT) {
-                        clearInterval(interval);
-                        reject(data);
-                        return;
-                    }
-                }, 1000);
-            } catch (ex) {
-                reject(ex);
-            }
+                // waiting for user consent, retry after 1000 ms
+            }, 1000);
         });
     }
 
@@ -154,8 +142,9 @@ export default class Revolut {
 
         if (this.tokenData && this.tokenData.user) {
             creds = Buffer.from(`${this.tokenData.user?.id}:${this.tokenData.accessToken}`, 'utf-8').toString('base64');
-            if (this.tokenData.refreshCode)
+            if (this.tokenData.refreshCode) {
                 refreshToken = Buffer.from(this.tokenData.refreshCode, 'utf-8').toString('base64');
+            }
         }
 
         this.credentials = creds;
@@ -175,37 +164,29 @@ export default class Revolut {
         };
     }
 
-    public updateToken(force: boolean = false): Promise<boolean> {
+    public async updateToken(force: boolean = false): Promise<void> {
         if (!force && this.tokenData.tokenExpiryDate && this.tokenData.tokenExpiryDate > Date.now()) {
-            return Promise.resolve(true);
+            return;
         }
-        return new Promise(async (resolve, reject) => {
-            try {
-                let payload: Requests.T_UpdateToken = {
-                    userId: this.tokenData.user?.id || '',
-                    refreshCode: this.tokenData.refreshCode || '',
-                };
-                let res = await this.axios.put(URLs.token, payload, this.getAxiosOptions());
-                let data: Responses.T_Token = res.data;
-                if (!data.refreshCode || !data.accessToken) {
-                    throw new Error('Unexpected Response. ');
-                    return;
-                }
 
-                this.refreshToken = data.refreshCode;
-                this.tokenData.accessToken = data.accessToken;
-                this.tokenData.refreshCode = data.refreshCode;
-                this.tokenData.tokenExpiryDate = data.tokenExpiryDate;
+        let payload: Requests.T_UpdateToken = {
+            userId: this.tokenData.user?.id || '',
+            refreshCode: this.tokenData.refreshCode || '',
+        };
+        let res = await this.axios.put(urls.token, payload, this.getAxiosOptions());
+        let data: Responses.T_Token = res.data;
+        if (!data.refreshCode || !data.accessToken) {
+            throw new Error('Unexpected Response');
+        }
 
-                this.configFn({
-                    ...this.configFn(),
-                    data: this.tokenData,
-                });
+        this.refreshToken = data.refreshCode;
+        this.tokenData.accessToken = data.accessToken;
+        this.tokenData.refreshCode = data.refreshCode;
+        this.tokenData.tokenExpiryDate = data.tokenExpiryDate;
 
-                resolve(true);
-            } catch (ex) {
-                reject(ex);
-            }
+        this.configFn({
+            ...this.configFn(),
+            data: this.tokenData,
         });
     }
 
@@ -215,46 +196,31 @@ export default class Revolut {
      * Otherwise, sets the token data, credentials, refresh token, and device ID from the configuration.
      * @returns Promise that resolves with a boolean indicating success or rejects with an error object.
      */
-    public signin(): Promise<boolean> {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let conf = this.configFn();
-                if (conf == null) {
-                    if (this.phoneNumber == null) {
-                        throw new Error("Phone number was not passed, but there is no logged-in session. " +
-                            "Phone number is needed to sign in for the first time.");
-                    }
-                    await this.setToken();
-                    this.tokenData = await this.waitForPublicToken();
-                } else {
-                    this.tokenData = conf.data;
-                    this.credentials = conf.credentials;
-                    this.refreshToken = conf.refreshToken;
-                    this.deviceId = conf.deviceId;
-                    await this.updateToken();
-                }
-                resolve(true);
-            } catch (ex) {
-                console.log(ex);
-                reject(ex);
+    public async signin(): Promise<void> {
+        let conf = this.configFn();
+        if (conf == null) {
+            if (this.phoneNumber == null) {
+                throw new Error("Phone number was not passed, but there is no logged-in session. " +
+                    "Phone number is needed to sign in for the first time.");
             }
-        });
+            await this.setToken();
+            this.tokenData = await this.waitForPublicToken();
+        } else {
+            this.tokenData = conf.data;
+            this.credentials = conf.credentials;
+            this.refreshToken = conf.refreshToken;
+            this.deviceId = conf.deviceId;
+            await this.updateToken();
+        }
     }
 
     /**
      * Retrieve all cards from the current revolut account, return a Promise that resolves to an object containing the data.
      */
-    public getCards(): Promise<Responses.T_All> {
-        return new Promise(async (resolve, reject) => {
-            await this.updateToken();
-            try {
-                let res = await this.axios(URLs.all, this.getAxiosOptions());
-                let data: Responses.T_All = res.data;
-                resolve(data);
-            } catch (ex) {
-                reject(ex);
-            }
-        });
+    public async getCards(): Promise<Responses.T_All> {
+        await this.updateToken();
+        let res = await this.axios(urls.all, this.getAxiosOptions());
+        return res.data;
     }
 
     /**
@@ -263,18 +229,10 @@ export default class Revolut {
      * @returns A Promise that resolves with the card details.
      * @throws An error if the card with the given UUID is not found.
      */
-    public getCard(uuid: string): Promise<Responses.T_FullCardDetails> {
-        return new Promise(async (resolve, reject) => {
-            await this.updateToken();
-            try {
-                let res = await this.axios.get(URLs.getCardDetailsURL(uuid), this.getAxiosOptions());
-                let data: Responses.T_FullCardDetails = res.data;
-
-                resolve(data);
-            } catch (ex) {
-                reject(ex);
-            }
-        });
+    public async getCard(uuid: string): Promise<Responses.T_FullCardDetails> {
+        await this.updateToken();
+        let res = await this.axios.get(urls.getCardDetailsURL(uuid), this.getAxiosOptions());
+        return res.data;
     }
 
     /**
@@ -283,20 +241,17 @@ export default class Revolut {
      * @returns A Promise that resolves with the decrypted card secrets.
      * @throws An error if the card is not found.
      */
-    public getCardSecrets(uuid: string): Promise<Responses.T_Details> {
-        return new Promise(async (resolve, reject) => {
-            await this.updateToken();
-            try {
-                let res = await this.axios.get(URLs.getCardDetailsURL(uuid), this.getAxiosOptions());
-                let data: Responses.T_Details = res.data;
-                if (!data.cvv || !data.pan) throw new Error("Card wasn't found");
-                data.cvv = this.decrypt(data.cvv);
-                data.pan = this.decrypt(data.pan);
-                resolve(data);
-            } catch (ex) {
-                reject(ex);
-            }
-        });
+    public async getCardSecrets(uuid: string): Promise<Responses.T_Details> {
+        await this.updateToken();
+        let res = await this.axios.get(urls.getCardDetailsURL(uuid), this.getAxiosOptions());
+        let data: Responses.T_Details = res.data;
+        if (!data.cvv || !data.pan) {
+            throw new Error("Card wasn't found");
+        }
+
+        data.cvv = this.decrypt(data.cvv);
+        data.pan = this.decrypt(data.pan);
+        return data;
     }
 
     /**
@@ -306,17 +261,10 @@ export default class Revolut {
      * @throws An error if the card details are not found.
      */
 
-    public deleteCard(uuid: string): Promise<Responses.T_FullCardDetails> {
-        return new Promise(async (resolve, reject) => {
-            await this.updateToken();
-            try {
-                let res = await this.axios.delete(URLs.getCardDeleteURL(uuid), this.getAxiosOptions());
-                let data: Responses.T_FullCardDetails = res.data;
-                resolve(data);
-            } catch (ex) {
-                reject(ex);
-            }
-        });
+    public async deleteCard(uuid: string): Promise<Responses.T_FullCardDetails> {
+        await this.updateToken();
+        let res = await this.axios.delete(urls.getCardDeleteURL(uuid), this.getAxiosOptions());
+        return res.data;
     }
 
     /**
@@ -326,23 +274,15 @@ export default class Revolut {
      * @returns {Promise<T_Responses['Details']>} - A Promise that resolves to the newly created virtual card details.
      * @throws {Error} - If the card details (CVV or PAN) are not found.
      */
-    public newVirtualCard(name: string): Promise<Responses.T_FullCardDetails> {
-        return new Promise(async (resolve, reject) => {
-            await this.updateToken();
-            try {
-                let payload: Requests.T_Issue = {
-                    label: name,
-                    design: 'LIGHT_GREEN_VIRTUAL',
-                    disposable: false,
-                };
+    public async newVirtualCard(name: string): Promise<Responses.T_FullCardDetails> {
+        await this.updateToken();
+        let payload: Requests.T_Issue = {
+            label: name,
+            design: 'LIGHT_GREEN_VIRTUAL',
+            disposable: false,
+        };
 
-                let res = await this.axios.post(URLs.issue_virtual, payload, this.getAxiosOptions());
-                let data: Responses.T_FullCardDetails = res.data;
-
-                resolve(data);
-            } catch (ex) {
-                reject(ex);
-            }
-        });
+        let res = await this.axios.post(urls.issue_virtual, payload, this.getAxiosOptions());
+        return res.data;
     }
 }
